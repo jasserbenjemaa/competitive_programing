ebf3feef9e0a58a69b3ee877635ce51d1cb9ccd4
Jasser <138148720+jasserbenjemaa@users.noreply.github.com>
2024-03-18 23:03:24 +0100

    Update julka_spoj.cpp

--- julka_spoj/julka_spoj.cpp
+++ julka_spoj/julka_spoj.cpp
@@ -67,19 +67,16 @@ string divide(string s)
 string add(string n1, string n2)
 {
 
-  // Make sure N2 is larger
   if (n1.length() > n2.length())
   {
     swap(n1, n2);
   }
 
-  // Result
   string result = "";
-  // Reverse
+  
   reverse(n1.begin(), n1.end());
   reverse(n2.begin(), n2.end());
 
-  // Add digit upto a n1.length()
   int carry = 0;
 
   for (int i = 0; i < n1.length(); i++)
@@ -102,13 +99,12 @@ string add(string n1, string n2)
     result.push_back(to_char(output_digit));
   }
 
-  // Finally if a carrry is generated
   if (carry)
   {
     result.push_back('1');
   }
 
-  // Reverse the final result
+
   reverse(result.begin(), result.end());
   return result;
 }
