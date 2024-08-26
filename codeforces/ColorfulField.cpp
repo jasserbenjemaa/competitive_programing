749a1086c6b9112533e398a371d9209c0e5683e4
jasser <jasserj663@gmail.com>
2024-08-26 20:45:10 +0100

    ColorfulField_B_79

--- codeforces/ColorfulField.cpp
+++ codeforces/ColorfulField.cpp
@@ -1,39 +1,45 @@
 #include <bits/stdc++.h>
 #include <csignal>
-#include <cstdint>
+#include <vector>
 using namespace std;
-#define int long long int
+int n, m, w, q;
+struct Point {
+  int x, y;
+};
 
-int32_t main() {
+int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
 #ifndef ONLINE_JUDGE
   freopen("../input.txt", "r", stdin);
   freopen("../output.txt", "w", stdout);
 #endif
-  int n, m, w, q;
   cin >> n >> m >> w >> q;
-  string mat[n][m], k[] = {"Carrots", "Kiwis", "Grapes"};
-  int x, y, t = 0;
+  vector<Point> arr(w);
+  string f[] = {"Grapes", "Carrots", "Kiwis"};
   for (int i = 0; i < w; i++) {
-    cin >> x >> y;
-    x--, y--;
-    mat[x][y] = "Waste";
-  }
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      if (mat[i][j] == "Waste")
-        continue;
-      if (t > 2)
-        t = 0;
-      mat[i][j] = k[t];
-      t++;
-    }
+    cin >> arr[i].y >> arr[i].x;
   }
   for (int i = 0; i < q; i++) {
-    cin >> x >> y;
-    x--, y--;
-    cout << mat[x][y] << endl;
+    bool isWaste = false;
+    Point p;
+    cin >> p.y >> p.x;
+    // Calculate the position in the flattened grid
+    int pos = (p.y - 1) * m + p.x;
+    for (int j = 0; j < w; j++) {
+      if (arr[j].y < p.y || (arr[j].x < p.x && arr[j].y == p.y)) {
+        pos--;
+      }
+      if (arr[j].y == p.y && arr[j].x == p.x) {
+        isWaste = true;
+        break;
+      }
+    }
+    if (isWaste)
+      cout << "Waste" << endl;
+    else
+      cout << f[pos % 3] << endl;
   }
+
   return 0;
 }
